d69dac9502e19e9169a9ec21d0ca01aec18df649
Malte Schwitters <malte.schwitters@outlook.de>
2018-09-03 15:44:38 +0200

    extracted footer

--- src/App.vue
+++ src/App.vue
@@ -10,33 +10,7 @@
 
                     <div class="flex-grow flex-1 ">
                         <router-view class="bg-white shadow-md"/>
-
-                        <div class="px-8 md:px-16 lg:px-24 py-16 bg-grey-darker">
-                            <div class="flex pb-20">
-                                <div class="w-1/2 text-sm">
-                                    <p class="text-white text-2xl">Unser Angebot</p>
-                                    <div class="text-white">🗸 <router-link class="text-white no-underline hover:underline" to="surfing#kurse">Windsurf Kurse</router-link></div>
-                                    <div class="text-white">🗸 <router-link class="text-white no-underline hover:underline" to="surfing#kurse">Surfmaterial Verleih</router-link></div>
-                                    <div class="text-white">🗸 <router-link class="text-white no-underline hover:underline" to="surfing#kurse">SUP Kurse</router-link></div>
-                                    <div class="text-white">🗸 <router-link class="text-white no-underline hover:underline" to="surfing#kurse">SUP Verleih</router-link></div>
-                                    <div class="text-white">🗸 <router-link class="text-white no-underline hover:underline" to="surfing#touren">Geführte SUP Touren</router-link></div>
-                                    <div class="text-white">🗸 <router-link class="text-white no-underline hover:underline" to="surfing#kurse">Segelkurse</router-link></div>
-                                    <div class="text-white">🗸 <router-link class="text-white no-underline hover:underline" to="surfing#kurse">Segelboot Verleih</router-link></div>
-                                </div>
-                                <div class="w-1/2 text-sm">
-                                    <p class="text-white text-2xl">Kontakt</p>
-                                    <p class="text-grey-light">Surfschule Grosses Meer</p>
-                                    <p class="text-grey-light">Forlitzer Str. 213</p>
-                                    <p class="text-grey-light">26624 Forlitz-Blaukirchen/ Südbrookmerland</p>
-                                    <a class="text-white no-underline hover:underline block" href="tel:049425096">04942 5096</a>
-                                    <a class="text-white no-underline hover:underline block" href="tel:01778879184">0177 887 91 84</a>
-                                    <a class="text-white no-underline hover:underline block" href="mailto:info@grosses-meer.surf">info@grosses-meer.surf</a>
-                                    <router-link class="text-white no-underline hover:underline block" to="impressum">Impressum</router-link>
-                                </div>
-                            </div>
-                            <p class="text-white text-center">Preise Stand 2018</p>
-                            <p class="text-white text-center">Copyright © 2018 Surfschule Grosses Meer</p>
-                        </div>
+                        <Footer></Footer>
                     </div>
 
                     <div class="xl:w-1/5"></div>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,29 @@
 <template>
-    <div class="bg-grey-dark flex">
-        <div class="flex-grow">
-            Unser Angebot
+    <div class="px-8 md:px-16 lg:px-24 py-16 bg-grey-darker">
+        <div class="flex pb-20">
+            <div class="w-1/2 text-sm">
+                <p class="text-white text-2xl">Unser Angebot</p>
+                <div class="text-white">🗸 <router-link class="text-white no-underline hover:underline" to="surfing#kurse">Windsurf Kurse</router-link></div>
+                <div class="text-white">🗸 <router-link class="text-white no-underline hover:underline" to="surfing#kurse">Surfmaterial Verleih</router-link></div>
+                <div class="text-white">🗸 <router-link class="text-white no-underline hover:underline" to="surfing#kurse">SUP Kurse</router-link></div>
+                <div class="text-white">🗸 <router-link class="text-white no-underline hover:underline" to="surfing#kurse">SUP Verleih</router-link></div>
+                <div class="text-white">🗸 <router-link class="text-white no-underline hover:underline" to="surfing#touren">Geführte SUP Touren</router-link></div>
+                <div class="text-white">🗸 <router-link class="text-white no-underline hover:underline" to="surfing#kurse">Segelkurse</router-link></div>
+                <div class="text-white">🗸 <router-link class="text-white no-underline hover:underline" to="surfing#kurse">Segelboot Verleih</router-link></div>
+            </div>
+            <div class="w-1/2 text-sm">
+                <p class="text-white text-2xl">Kontakt</p>
+                <p class="text-grey-light">Surfschule Grosses Meer</p>
+                <p class="text-grey-light">Forlitzer Str. 213</p>
+                <p class="text-grey-light">26624 Forlitz-Blaukirchen/ Südbrookmerland</p>
+                <a class="text-white no-underline hover:underline block" href="tel:049425096">04942 5096</a>
+                <a class="text-white no-underline hover:underline block" href="tel:01778879184">0177 887 91 84</a>
+                <a class="text-white no-underline hover:underline block" href="mailto:info@grosses-meer.surf">info@grosses-meer.surf</a>
+                <router-link class="text-white no-underline hover:underline block" to="impressum">Impressum</router-link>
+            </div>
         </div>
-        <div class="flex-grow">
-            Kontakt
-        </div>
+        <p class="text-white text-center">Preise Stand 2018</p>
+        <p class="text-white text-center">Copyright © 2018 Surfschule Grosses Meer</p>
     </div>
 </template>
 
@@ -16,18 +34,6 @@
         name: "Footer"
     })
     export default class Footer extends Vue {
-
-        private activeTab: any;
-
-        private async created() {
-            this.activeTab = this.$router.currentRoute.name;
-            this.$router.beforeResolve((to, from, next) => {
-                this.activeTab = to.name;
-                this.$nextTick(() => {
-                    this.$forceUpdate()
-                });
-                next();
-            });
-        }
-     }
+        
+    }
 </script>
