60bc2e043961a0797d295a92563ba89eda5dff14
Malte Schwitters <malte.schwitters@outlook.de>
2018-09-16 13:35:45 +0200

    fixed links width

--- src/views/Weather.vue
+++ src/views/Weather.vue
@@ -36,12 +36,12 @@
         </div>
 
         <!--<div class="px-6 md:px-16 lg:px-24 py-6">-->
-            <!--<a id="stats" class="anchor"></a>-->
-            <!--<h1 class="pt-8 text-2xl xl:text-3xl">Wind Statistiken</h1>-->
-            <!--<div class="w-24 my-4 border-grey-light border-b"></div>-->
-            <!--<p class="pb-6 text-base xl:text-lg">-->
-                <!--Coming Soon-->
-            <!--</p>-->
+        <!--<a id="stats" class="anchor"></a>-->
+        <!--<h1 class="pt-8 text-2xl xl:text-3xl">Wind Statistiken</h1>-->
+        <!--<div class="w-24 my-4 border-grey-light border-b"></div>-->
+        <!--<p class="pb-6 text-base xl:text-lg">-->
+        <!--Coming Soon-->
+        <!--</p>-->
         <!--</div>-->
 
         <div class="p-2">
@@ -50,63 +50,65 @@
                 <h1 class="pt-8 text-2xl xl:text-3xl">Wetterberichte</h1>
                 <div class="w-24 my-4 border-grey-light border-b"></div>
                 <p class="pb-6 text-base xl:text-lg">
-                    Keine Information ist wichtiger für einen Windsurfer, als die aktuelle Windvorhersage. Da jeder Wetterbericht
-                    sich auch mal irren kann, sollte man immer mehrere checken. Um euch langes suchen zu ersparen, haben wir
+                    Keine Information ist wichtiger für einen Windsurfer, als die aktuelle Windvorhersage. Da jeder
+                    Wetterbericht
+                    sich auch mal irren kann, sollte man immer mehrere checken. Um euch langes suchen zu ersparen, haben
+                    wir
                     euch hier Links zu den wichtigsten Wetterberichten zusammengestellt:
                 </p>
-                <div class="pb-6 w-64 overflow-hidden">
-                    <div class="table-row w-full">
-                        <p class="text-base xl:text-lg table-cell pr-4">Windguru: </p>
-                        <a class="text-base xl:text-lg table-cell no-underline hover:underline"
-                           href="https://www.windguru.cz/106165" target="_blank">
-                            https://www.windguru.cz
-                        </a>
-                    </div>
-                    <div class="table-row">
-                        <p class="text-base xl:text-lg table-cell pr-4">Windfinder: </p>
-                        <a class="text-base xl:text-lg table-cell no-underline hover:underline"
-                           href="https://www.windfinder.com/forecast/grosses_meer" target="_blank">
-                            https://www.windfinder.com
-                        </a>
-                    </div>
-                    <div class="table-row">
-                        <p class="text-base xl:text-lg table-cell pr-4">Meteomedia: </p>
-                        <a class="text-base xl:text-lg table-cell no-underline hover:underline"
-                           href="http://wetterstationen.meteomedia.de/?station=102000&wahl=vorhersage" target="_blank">
-                            http://wetterstationen.meteomedia.de
-                        </a>
-                    </div>
-                    <div class="table-row">
-                        <p class="text-base xl:text-lg table-cell pr-4">Friesenwetter: </p>
-                        <a class="text-base xl:text-lg table-cell no-underline hover:underline"
-                           href="http://www.friesenwetter.de/wettervorhersage" target="_blank">
-                            http://www.friesenwetter.de
-                        </a>
-                    </div>
-                    <div class="table-row">
-                        <p class="text-base xl:text-lg table-cell pr-4">Wisuki: </p>
-                        <a class="text-base xl:text-lg table-cell no-underline hover:underline"
-                           href="http://wisuki.com/spot/1529/grosses-meer" target="_blank">
-                            http://wisuki.com
-                        </a>
-                    </div>
-                    <div class="table-row">
-                        <p class="text-base xl:text-lg table-cell pr-4">Regenradar: </p>
-                        <a class="text-base xl:text-lg table-cell no-underline hover:underline"
-                           href="https://www.wetteronline.de/regenradar/niedersachsen" target="_blank">
-                            https://www.wetteronline.de
-                        </a>
-                    </div>
+
+                <div class="flex">
+                    <p class="w-1/3 text-base xl:text-lg pr-4">Windguru: </p>
+                    <a class="w-2/3 text-base xl:text-lg no-underline hover:underline"
+                       href="https://www.windguru.cz/106165" target="_blank">
+                        www.windguru.cz
+                    </a>
+                </div>
+                <div class="flex">
+                    <p class="w-1/3 text-base xl:text-lg pr-4">Windfinder: </p>
+                    <a class="w-2/3 text-base xl:text-lg no-underline hover:underline"
+                       href="https://www.windfinder.com/forecast/grosses_meer" target="_blank">
+                        www.windfinder.com
+                    </a>
+                </div>
+                <div class="flex">
+                    <p class="w-1/3 text-base xl:text-lg pr-4">Meteomedia: </p>
+                    <a class="w-2/3 text-base xl:text-lg no-underline hover:underline"
+                       href="http://wetterstationen.meteomedia.de/?station=102000&wahl=vorhersage" target="_blank">
+                        wetterstationen.meteomedia.de
+                    </a>
+                </div>
+                <div class="flex">
+                    <p class="w-1/3 text-base xl:text-lg pr-4">Friesenwetter: </p>
+                    <a class="w-2/3 text-base xl:text-lg no-underline hover:underline"
+                       href="http://www.friesenwetter.de/wettervorhersage" target="_blank">
+                        www.friesenwetter.de
+                    </a>
+                </div>
+                <div class="flex">
+                    <p class="w-1/3 text-base xl:text-lg pr-4">Wisuki: </p>
+                    <a class="w-2/3 text-base xl:text-lg no-underline hover:underline"
+                       href="http://wisuki.com/spot/1529/grosses-meer" target="_blank">
+                        wisuki.com
+                    </a>
+                </div>
+                <div class="flex">
+                    <p class="w-1/3 text-base xl:text-lg pr-4">Regenradar: </p>
+                    <a class="w-2/3 text-base xl:text-lg no-underline hover:underline"
+                       href="https://www.wetteronline.de/regenradar/niedersachsen" target="_blank">
+                        www.wetteronline.de
+                    </a>
                 </div>
             </div>
         </div>
     </div>
+    </div>
 </template>
 
 <script lang="ts">
-    import { Component, Vue } from 'vue-property-decorator';
+    import {Component, Vue} from 'vue-property-decorator';
 
-    @Component({ components: {} })
+    @Component({components: {}})
     export default class Weather extends Vue {
 
     }
